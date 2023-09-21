4cd5e5f9191162797bdd0881346da29f6f14414c
matissolus <matiss2000@inbox.lv>
2023-09-21 11:25:20 +0100

    w.i.p pop-up warning

--- scripts/Game.cs
+++ scripts/Game.cs
@@ -3,196 +3,196 @@ using System;
 
 public partial class Game : Node3D
 {
-    // Called when the node enters the scene tree for the first time.
-    bool Menu = true;
-    bool Playing = false;
-    Camera3D MainCam;
-    Vector3 CamPlayPos, CamPlayRot, CamMenuPos, CamMenuRot;
-    RandomNumberGenerator RNG;
-    SaveManager SaveManager;
+	// Called when the node enters the scene tree for the first time.
+	bool Menu = true;
+	bool Playing = false;
+	Camera3D MainCam;
+	Vector3 CamPlayPos, CamPlayRot, CamMenuPos, CamMenuRot;
+	RandomNumberGenerator RNG;
+	SaveManager SaveManager;
 
-    Mallet Mallet;
-    Mole Mole;
-    PanelContainer MainMenu;
-    Control GameOverMenu;
-    Button PlayResume;
-    VBoxContainer MenuButtons, HelpMenu, SettingsMenu;
-    Timer ComboCointTimer;
+	Mallet Mallet;
+	Mole Mole;
+	PanelContainer MainMenu;
+	Control GameOverMenu;
+	Button PlayResume;
+	VBoxContainer MenuButtons, HelpMenu, SettingsMenu;
+	Timer ComboCointTimer;
 
-    Timer PopOutTimer;
+	Timer PopOutTimer;
 
-    PackedScene Coin;
+	PackedScene Coin;
 
-    public bool MenuJustPressed { get; private set; }
+	public bool MenuJustPressed { get; private set; }
 
-    public override void _Ready()
-    {
-        this.SaveManager = GetTree().Root.GetNode<SaveManager>("SaveManager");
-        this.SaveManager.LoadConfig();
+	public override void _Ready()
+	{
+		this.SaveManager = GetTree().Root.GetNode<SaveManager>("SaveManager");
+		this.SaveManager.LoadConfig();
 
-        RNG = new RandomNumberGenerator();
-        MenuButtons = GetNode<VBoxContainer>("%MenuButtons");
-        HelpMenu = GetNode<VBoxContainer>("%HelpScreen");
-        SettingsMenu = GetNode<VBoxContainer>("%SettingsMenu");
-        MainMenu = GetNode<PanelContainer>("%Intro");
-        MainCam = GetNode<Camera3D>("Camera3D");
-        Mallet = GetNode<Mallet>("%Mallet");
-        Mole = GetNode<Mole>("%Mole");
-        GameOverMenu = GetNode<Control>("%GameOver");
-        PlayResume = GetNode<Button>("%PlayResume");
-        ComboCointTimer = GetNode<Timer>("%ComboCoinTimer");
-        PopOutTimer = GetNode<Timer>("%PopOutTimer");
+		RNG = new RandomNumberGenerator();
+		MenuButtons = GetNode<VBoxContainer>("%MenuButtons");
+		HelpMenu = GetNode<VBoxContainer>("%HelpScreen");
+		SettingsMenu = GetNode<VBoxContainer>("%SettingsMenu");
+		MainMenu = GetNode<PanelContainer>("%Intro");
+		MainCam = GetNode<Camera3D>("Camera3D");
+		Mallet = GetNode<Mallet>("%Mallet");
+		Mole = GetNode<Mole>("%Mole");
+		GameOverMenu = GetNode<Control>("%GameOver");
+		PlayResume = GetNode<Button>("%PlayResume");
+		ComboCointTimer = GetNode<Timer>("%ComboCoinTimer");
+		PopOutTimer = GetNode<Timer>("%PopOutTimer");
 
 
-        Coin = ResourceLoader.Load<PackedScene>("scenes/Coin.tscn");
+		Coin = ResourceLoader.Load<PackedScene>("scenes/Coin.tscn");
 
-        CamPlayPos = new Vector3(0, 2.403f, 1.516f);
-        CamPlayRot = new Vector3(-36.8f, 0, 0);
+		CamPlayPos = new Vector3(0, 2.403f, 1.516f);
+		CamPlayRot = new Vector3(-36.8f, 0, 0);
 
-        // CamMenuPos = new Vector3(0, 1.621f, 0.667f); //old menu
-        // CamMenuRot = new Vector3(0, 30, 0);         //old menu
-        CamMenuPos = new Vector3(-3.25f, 1.53f, -0.9f); //old menu
-        CamMenuRot = new Vector3(0, 90, 0);         //old menu
-        PlayResume.Text = "Play";
-        MainCam.Position = CamMenuPos;
-    }
+		// CamMenuPos = new Vector3(0, 1.621f, 0.667f); //old menu
+		// CamMenuRot = new Vector3(0, 30, 0);         //old menu
+		CamMenuPos = new Vector3(-3.25f, 1.53f, -0.9f); //old menu
+		CamMenuRot = new Vector3(0, 90, 0);         //old menu
+		PlayResume.Text = "Play";
+		MainCam.Position = CamMenuPos;
+	}
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public async override void _Process(double delta)
-    {
-        if (Input.IsActionJustReleased("menu") && !Menu && !Mole.GetGameOver())
-        {
-            this.SaveManager.SaveConfig();
-            if (!MenuJustPressed)
-            {
-                MenuJustPressed = true;
-                MoveCamera(CamMenuPos, CamMenuRot);
-                PlayResume.Text = "Resume";
-                Menu = true;
-                Mole.Paused = true;
-                PopOutTimer.Paused = true;
-                await ToSignal(GetTree().CreateTimer(1.5f), "timeout");
-                MainMenu.Show();
-                MenuJustPressed = false;
-            }
-        }
-        else if (Input.IsActionJustReleased("menu") && Menu)
-        {
-            this.SaveManager.SaveConfig();
-            if (!MenuJustPressed)
-            {
-                MenuJustPressed = true;
-                MainMenu.Hide();
-                MoveCamera(CamPlayPos, CamPlayRot);
-                Menu = false;
-                await ToSignal(GetTree().CreateTimer(1.5f), "timeout");
-                PopOutTimer.Paused = false;
-                Mole.Paused = false;
-                MenuJustPressed = false;
-            }
-        }
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public async override void _Process(double delta)
+	{
+		if (Input.IsActionJustReleased("menu") && !Menu && !Mole.GetGameOver())
+		{
+			this.SaveManager.SaveConfig();
+			if (!MenuJustPressed)
+			{
+				MenuJustPressed = true;
+				MoveCamera(CamMenuPos, CamMenuRot);
+				PlayResume.Text = "Resume";
+				Menu = true;
+				Mole.Paused = true;
+				PopOutTimer.Paused = true;
+				await ToSignal(GetTree().CreateTimer(1.5f), "timeout");
+				MainMenu.Show();
+				MenuJustPressed = false;
+			}
+		}
+		else if (Input.IsActionJustReleased("menu") && Menu)
+		{
+			this.SaveManager.SaveConfig();
+			if (!MenuJustPressed)
+			{
+				MenuJustPressed = true;
+				MainMenu.Hide();
+				MoveCamera(CamPlayPos, CamPlayRot);
+				Menu = false;
+				await ToSignal(GetTree().CreateTimer(1.5f), "timeout");
+				PopOutTimer.Paused = false;
+				Mole.Paused = false;
+				MenuJustPressed = false;
+			}
+		}
 
-        if (Mole.GetGameOver())
-        {
-            GameOverMenu.Show();
-            Menu = false;
-            if (Input.IsActionJustReleased("menu"))
-            {
-                this.SaveManager.SaveConfig();
-                Mole.SetGameOver(false);
-                Mole.Playing = false;
-                GameOverMenu.Hide();
-                MoveCamera(CamMenuPos, CamMenuRot);
-                PlayResume.Text = "Play";
-                await ToSignal(GetTree().CreateTimer(1.5f), "timeout");
-                MainMenu.Show();
-                Menu = true;
-            }
-        }
-    }
+		if (Mole.GetGameOver())
+		{
+			GameOverMenu.Show();
+			Menu = false;
+			if (Input.IsActionJustReleased("menu"))
+			{
+				this.SaveManager.SaveConfig();
+				Mole.SetGameOver(false);
+				Mole.Playing = false;
+				GameOverMenu.Hide();
+				MoveCamera(CamMenuPos, CamMenuRot);
+				PlayResume.Text = "Play";
+				await ToSignal(GetTree().CreateTimer(1.5f), "timeout");
+				MainMenu.Show();
+				Menu = true;
+			}
+		}
+	}
 
-    public void _on_restart_pressed()
-    {
-        Mole.Playing = true;
-        Mole.Restart();
-        ComboCointTimer.Start(RNG.RandiRange(3, 6));
-        Mole.SetGameOver(false);
-        Mole.Paused = false;
-        GameOverMenu.Hide();
-        Menu = false;
-    }
-    public void _on_main_menu_pressed()
-    {
-        Mole.SetGameOver(false);
-        Mole.Playing = false;
-        Mole.Restart();
-        GameOverMenu.Hide();
-        MainMenu.Show();
-        MoveCamera(CamMenuPos, CamMenuRot);
-        PlayResume.Text = "Play";
-        Menu = true;
-    }
+	public void _on_restart_pressed()
+	{
+		Mole.Playing = true;
+		Mole.Restart();
+		ComboCointTimer.Start(RNG.RandiRange(3, 6));
+		Mole.SetGameOver(false);
+		Mole.Paused = false;
+		GameOverMenu.Hide();
+		Menu = false;
+	}
+	public void _on_main_menu_pressed()
+	{
+		Mole.SetGameOver(false);
+		Mole.Playing = false;
+		Mole.Restart();
+		GameOverMenu.Hide();
+		MainMenu.Show();
+		MoveCamera(CamMenuPos, CamMenuRot);
+		PlayResume.Text = "Play";
+		Menu = true;
+	}
 
-    //play/resume button
-    public async void _on_button_pressed()
-    {
-        if (!Mole.Paused)
-        {
-            Mole.Restart();
-            ComboCointTimer.Start(RNG.RandiRange(3, 6));
+	//play/resume button
+	public async void _on_button_pressed()
+	{
+		if (!Mole.Paused)
+		{
+			Mole.Restart();
+			ComboCointTimer.Start(RNG.RandiRange(3, 6));
 
-            Mole.Paused = false;
-        }
-        MainMenu.Hide();
-        ComboCointTimer.Start(RNG.RandiRange(3, 6));
-        MoveCamera(CamPlayPos, CamPlayRot);
-        await ToSignal(GetTree().CreateTimer(2f), "timeout");
-        Mole.Playing = true;
-        Mole.Paused = false;
-        Menu = false;
-    }
-    public void _on_settings_pressed()
-    {
-        SettingsMenu.Show();
-        MenuButtons.Hide();
-    }
+			Mole.Paused = false;
+		}
+		MainMenu.Hide();
+		ComboCointTimer.Start(RNG.RandiRange(3, 6));
+		MoveCamera(CamPlayPos, CamPlayRot);
+		await ToSignal(GetTree().CreateTimer(2f), "timeout");
+		Mole.Playing = true;
+		Mole.Paused = false;
+		Menu = false;
+	}
+	public void _on_settings_pressed()
+	{
+		SettingsMenu.Show();
+		MenuButtons.Hide();
+	}
 
-    public void _on_settings_back_pressed()
-    {
-        this.SaveManager.SaveConfig();
-        SettingsMenu.Hide();
-        MenuButtons.Show();
-    }
+	public void _on_settings_back_pressed()
+	{
+		this.SaveManager.SaveConfig();
+		SettingsMenu.Hide();
+		MenuButtons.Show();
+	}
 
-    public void _on_help_pressed()
-    {
-        MenuButtons.Hide();
-        HelpMenu.Show();
-    }
-    public void _on_back_help_pressed()
-    {
-        MenuButtons.Show();
-        HelpMenu.Hide();
-    }
+	public void _on_help_pressed()
+	{
+		MenuButtons.Hide();
+		HelpMenu.Show();
+	}
+	public void _on_back_help_pressed()
+	{
+		MenuButtons.Show();
+		HelpMenu.Hide();
+	}
 
-    public void MoveCamera(Vector3 Pos, Vector3 Rot)
-    {
-        Tween MoveCam = GetTree().CreateTween();
-        MoveCam.TweenProperty(MainCam, "position", Pos, 1.5f).SetTrans(Tween.TransitionType.Cubic).SetEase(Tween.EaseType.In);
-        Tween RotCam = GetTree().CreateTween();
-        RotCam.TweenProperty(MainCam, "rotation_degrees", Rot, 1.5f).SetTrans(Tween.TransitionType.Cubic).SetEase(Tween.EaseType.In);
-    }
+	public void MoveCamera(Vector3 Pos, Vector3 Rot)
+	{
+		Tween MoveCam = GetTree().CreateTween();
+		MoveCam.TweenProperty(MainCam, "position", Pos, 1.5f).SetTrans(Tween.TransitionType.Cubic).SetEase(Tween.EaseType.In);
+		Tween RotCam = GetTree().CreateTween();
+		RotCam.TweenProperty(MainCam, "rotation_degrees", Rot, 1.5f).SetTrans(Tween.TransitionType.Cubic).SetEase(Tween.EaseType.In);
+	}
 
-    public void _on_exit_pressed()
-    {
-        this.SaveManager.SaveConfig();
-        GetTree().Quit();
-    }
+	public void _on_exit_pressed()
+	{
+		this.SaveManager.SaveConfig();
+		GetTree().Quit();
+	}
 
-    public void _on_combo_coin_timer_timeout()
-    {
-        var coinInstance = Coin.Instantiate<Area3D>();
-        AddChild(coinInstance);
-        ComboCointTimer.Start(4);
-    }
+	public void _on_combo_coin_timer_timeout()
+	{
+		var coinInstance = Coin.Instantiate<Area3D>();
+		AddChild(coinInstance);
+		ComboCointTimer.Start(4);
+	}
 }
